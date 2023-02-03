4e7808e81e14589601a12daa498948c4e09fb7ca
Luiz Felipe <lffm1994@gmail.com>
2023-02-02 22:58:25 -0300

    Implementando uma funcionalidade com TDD

---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReajusteServiceTest  {
+
+ /*
+    O sistema deve permitir que os funcionários recebam um reajuste salarial anual baseado em seu desempenho, obedecendo às seguintes regras:
+
+    Se o desempenho for classificado como "A desejar", o reajuste será de 3% do salário atual.
+    Se o desempenho for "Bom,", o reajuste será de 15% do salário.
+    Se o desempenho for "Ótimo", o reajuste será de 20% do salário.
+ */
+
+    @Test
+    public void reajusteDeveriaSerDeTresPorcentoQuandoDesempenhoForADesejar() {
+        ReajusteService service = new ReajusteService();
+        Funcionario funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+
+        service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+    }
+}
