eed838ebc0e6422f3872b4ace4edcfe4ea62f81b
Luiz Felipe <lffm1994@gmail.com>
2023-02-02 23:54:08 -0300

    Testando um método que lança exception

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import br.com.alura.tdd.modelo.Funcionario;
 
@@ -9,9 +10,10 @@ public class BonusService {
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
+			throw new IllegalArgumentException("Funcionário com salário maior do que R$10000 nao pode receber bonus!");
+
 		}
-		return valor;
+		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
 
 }
