23a06f2886aa9994c8b7a364a5a5cb11fe2b1a2a
Luiz Felipe <lffm1994@gmail.com>
2023-02-02 21:07:24 -0300

    Escrevendo um teste automatizado

---
+++ src/test/java/br/com/alura/tdd/modelo/CalculadoraTest.java
@@ -0,0 +1,39 @@
+package br.com.alura.tdd.modelo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+    private static Calculadora calculadora;
+
+    @BeforeAll
+    public static void beforeClassTest() {
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void testeSomaCalculadora1() {
+
+        int soma = calculadora.somar(3, 7);
+
+        Assertions.assertEquals(10, soma);
+    }
+
+    @Test
+    public void testeSomaCalculadora2() {
+
+        int soma = calculadora.somar(3, 0);;
+
+        Assertions.assertEquals(3, soma);
+    }
+
+    @Test
+    public void testeSomaCalculadora3() {
+
+        int soma = calculadora.somar(3, -1);
+
+        Assertions.assertEquals(2, soma);
+    }
+}
